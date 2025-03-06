cd13f7d35e8140ad856362205b5087b25b454bc5
sumit kumar <sumit9354800@gmail.com>
2025-03-06 14:58:32 +0530

    reducer concept

--- self-project/todos/src/App.jsx
+++ self-project/todos/src/App.jsx
@@ -1,62 +1,24 @@
-import { useState } from 'react'
 import './App.css'
 import ItemInput from './component/input'
 import AddNewList from './component/addNewList';
 import Welecome from './component/welcom';
-import ContextItem from './store/item-store-context';
+import { ContextItemProvider } from './store/item-store-context';
+
+
 
 
 function App() {
 
-
-   const [currentItem,setItem] = useState([]);
-
-    function addItem(item,date){
-      const newTodoItem = [
-        ...currentItem,{name:item , date:date}
-      ];
-      setItem(newTodoItem);
-    }
-
-    // or pr key unique/data/value honi chahiye
-    // function handleList(item,date){
-    //   setItem((currValue) =>[
-    //     ...currValue,{name:item , date:date}
-    //   ]);
-    // }
-
-   function deleteItem(todoItem){
-    // const newTodoItems = currentItem.filter((item) => item.name !== todoItem)
-                    //  OR 
-  // is mai jo bhi not equal hai vo sab value array se mil jayegi aur equal wali hat jayegi
-    const newTodoItems = currentItem.filter(item => item.name !== todoItem)
-    setItem(newTodoItems);
-   }
-
-   
-
   return (
     <>
-      <ContextItem.Provider value={{
-       currentItem,
-       addItem,
-       deleteItem,
-
-      //  or
-
-      /* items: currentItem,
-       addItem:addItem,
-       deleteItem:deleteItem,*/
-
-        }}> 
-      <div>
-        <h1 className='text-center bg-info p-2'>Todo List</h1>
-       <ItemInput/> 
-       {<Welecome />}
-        <AddNewList/>
-       
-      </div>
-      </ContextItem.Provider>
+      <ContextItemProvider>
+        <div>
+          <h1 className='text-center bg-info p-2'>Todo List</h1>
+          <ItemInput />
+          {<Welecome />}
+          <AddNewList />
+        </div>
+      </ContextItemProvider>
     </>
   )
 }
--- self-project/todos/src/store/item-store-context.jsx
+++ self-project/todos/src/store/item-store-context.jsx
@@ -1,11 +1,77 @@
 import { createContext } from "react";
+import { useReducer } from 'react'
 
-const ContextItem = createContext(
+export const ContextItem = createContext(
     {
-        currentItem:[],
-        addItem: ()=>{},
-        deleteItem: ()=>{},
-    } 
+        currentItem: [],
+        addItem: () => { },
+        deleteItem: () => { },
+    }
 );
+
+const reducer = (currentState, action) => {
+
+    let newTodoItem = currentState;
+
+    if (action.type == "ADD_ITEM") {
+        newTodoItem = [
+            ...currentState, { name: action.payload.item, date: action.payload.date }
+        ];
+
+    } else if (action.type == "DELETE_ITEM") {
+        newTodoItem = currentState.filter(item => item.name !== action.payload.todoItem)
+    }
+    return newTodoItem;
+}
+
+
+const ContextItemProvider = ({ children }) => {
+
+    const [currentItem, dispatchItem] = useReducer(reducer, []);
+
+    function addItem(item, date) {
+
+        const AddStateItem = {
+            type: "ADD_ITEM",
+            payload: {
+                item,
+                date,
+            }
+        }
+        dispatchItem(AddStateItem);
+    }
+
+
+    function deleteItem(todoItem) {
+        const deleteStateItem = {
+            type: "DELETE_ITEM",
+            payload: {
+                todoItem,
+            }
+        }
+        dispatchItem(deleteStateItem)
+
+    }
+
+    return <ContextItem.Provider value={{
+        currentItem,
+        addItem,
+        deleteItem,
+
+        //  or
+
+        /* items: currentItem,
+         addItem:addItem,
+         deleteItem:deleteItem,*/
+
+    }}>
+
+        {children}
+
+    </ContextItem.Provider>
+
+}
+
+export { ContextItemProvider };
 
 export default ContextItem;
